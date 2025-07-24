343ede4b639ced4b27e2a13535bb59d453b8149e
NNA Design <nabilahatiqah12@gmail.com>
2025-07-24 20:21:31 +0800

    Update n98hm1a2n.js

--- Asyiqin_Chairil/n98hm1a2n.js
+++ Asyiqin_Chairil/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.2136956238"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.64141481"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
