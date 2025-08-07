e8d49942537b21df9f813d1e1cf8b475b0097a47
NNA Design <nabilahatiqah12@gmail.com>
2025-08-07 17:49:02 +0800

    Update n98hm1a2n.js

--- Walimatulurus_Nazrin-Atikah/n98hm1a2n.js
+++ Walimatulurus_Nazrin-Atikah/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.416455188"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
