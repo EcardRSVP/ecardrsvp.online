4ce93b57ef8f541a87989c99cde012c9b4735aab
NNA Design <nabilahatiqah12@gmail.com>
2025-08-11 01:02:15 +0800

    Update n98hm1a2n.js

--- Azlan-Lela/n98hm1a2n.js
+++ Azlan-Lela/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.285747650"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.742930332"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
