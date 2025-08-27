791ce0f289588c17a399c7f17bde7ff21f0b37a2
NNA Design <nabilahatiqah12@gmail.com>
2025-08-28 02:35:30 +0800

    Update n98hm1a2n.js

--- Daniel-Mardhiya/n98hm1a2n.js
+++ Daniel-Mardhiya/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.1530875765"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
@@ -140,7 +140,7 @@ function toggleSection(id) {
 }
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSZnCEUZgn5DILxN3OSqM6MYdM06_FtgB3mK8rQXFpjax-yJXtq3_-hfcbH5pMaJzyUWAl2k_TMps2m/pub?gid=2108277645&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
