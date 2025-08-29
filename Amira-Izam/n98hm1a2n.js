239db9f4cbeaac2110b08989e0ae262611e7d49b
NNA Design <nabilahatiqah12@gmail.com>
2025-08-30 01:53:09 +0800

    Update n98hm1a2n.js

--- Amira-Izam/n98hm1a2n.js
+++ Amira-Izam/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.94294691"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
@@ -138,27 +138,6 @@ function toggleSection(id) {
     target.scrollIntoView({ behavior: "smooth" });
   }
 }
-
-// ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
-  .then(response => response.text())
-  .then(data => {
-    const parsed = Papa.parse(data, { header: true });
-    const ucapanList = document.getElementById("ucapanList");
-
-    if (ucapanList) {
-      ucapanList.innerHTML = "";
-      parsed.data.forEach(row => {
-        const nama = row["Nama"]?.trim();
-        const ucapan = row["Ucapan"]?.trim();
-        if (nama && ucapan) {
-          const item = document.createElement("p");
-          item.innerHTML = `<strong>${nama}</strong>: ${ucapan}`;
-          ucapanList.appendChild(item);
-        }
-      });
-    }
-  });
 
 // SENARAI ID popup & ikon yang berkaitan
 const popupMap = {
