281c3d26e2d6e6f5f731f26f35c573cc96f70a8e
NNA Design <nabilahatiqah12@gmail.com>
2025-08-30 03:51:53 +0800

    Update n98hm1a2n.js

--- Yusroh-Hafis/n98hm1a2n.js
+++ Yusroh-Hafis/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.337783190"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
@@ -140,7 +140,7 @@ function toggleSection(id) {
 }
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSB0jcBLWvENV287OlqD7aJP0vv8CQkTG0xDhTrdkt6uFAyI2mX0yODeFiU_Xitbl4xVVJPgtxOmlCZ/pub?gid=943066890&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
