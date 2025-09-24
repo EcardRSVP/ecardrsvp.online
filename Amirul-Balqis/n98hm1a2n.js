2ee6b4cad544c01425f194969cee30299a28e297
NNA Design <nabilahatiqah12@gmail.com>
2025-09-24 22:33:45 +0800

    Update n98hm1a2n.js

--- Amirul-Balqis/n98hm1a2n.js
+++ Amirul-Balqis/n98hm1a2n.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 📨 Validasi RSVP sebelum hantar
   if (form && popup) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.1711974082"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.1609028998"]:checked');
 
 if (!nama.value.trim() || !kehadiran) {
   e.preventDefault();
