c6fdb959c86172dbdd2d0134598b844671743839
NNA Design <nabilahatiqah12@gmail.com>
2025-09-26 13:04:31 +0800

    Update n98hm1a2n.js

--- WalimatulurusHaikal-Liya/n98hm1a2n.js
+++ WalimatulurusHaikal-Liya/n98hm1a2n.js
@@ -1,20 +1,3 @@
-// ✅ Tanda tempahan "Bakul Baju" jika sudah dipilih di Google Sheet
-fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTPi4cVVJVAYtYrDQRfhBMX0qCMllBMgjYqesb64WKf-5M4BvxIrabnse_Fq_Iu6EHsrnI8Rv1AEv7T/pub?output=csv')
-  .then(response => response.text())
-  .then(data => {
-    const rows = data.split("\n").map(row => row.split(","));
-    rows.forEach(row => {
-      const barang = row[2].trim();
-      if (barang === "Bakul Baju") {
-        const btn = document.getElementById("btn-bakul");
-        if (btn) {
-          btn.innerText = "Telah Ditempah";
-          btn.disabled = true;
-        }
-      }
-    });
-  });
-
 // ✅ Fungsi Salji Jatuh
 function mulakanSalji() {
   const wrapper = document.getElementById("snow-wrapper");
