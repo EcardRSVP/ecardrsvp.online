5c1cfa605f65c5a564e2ef1a607dacf8ad9a0638
NNA Design <nabilahatiqah12@gmail.com>
2025-10-14 00:43:50 +0800

    Update n98hm1a2n.js

--- cuba1/n98hm1a2n.js
+++ cuba1/n98hm1a2n.js
@@ -11,3 +11,23 @@ function closePopup() {
   popup.style.display = 'none';
 }
 </script>
+
+// slider.js
+const images = ["CodeA.png", "CodeB.png", "CodeC.png", "CodeD.png"];
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slider = document.getElementById("slider-image");
+  if(index < 0) currentIndex = images.length - 1;
+  else if(index >= images.length) currentIndex = 0;
+  else currentIndex = index;
+  slider.src = images[currentIndex];
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
