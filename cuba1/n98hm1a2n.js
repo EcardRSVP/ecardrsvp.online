f77e2f39945fae139dbda1840f7683d718ddb67f
NNA Design <nabilahatiqah12@gmail.com>
2025-10-13 22:40:17 +0800

    Update n98hm1a2n.js

--- cuba1/n98hm1a2n.js
+++ cuba1/n98hm1a2n.js
@@ -1,9 +1,13 @@
 <script>
-function openPopup(id) {
-  document.getElementById(id).style.display = "flex";
+function openPopup(imgSrc) {
+  const popup = document.getElementById('popupOverlay');
+  const popupImg = document.getElementById('popupImage');
+  popupImg.src = imgSrc;
+  popup.style.display = 'flex';   // pastikan popup nampak
 }
 
-function closePopup(id) {
-  document.getElementById(id).style.display = "none";
+function closePopup() {
+  const popup = document.getElementById('popupOverlay');
+  popup.style.display = 'none';
 }
 </script>
