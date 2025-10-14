5dff2367e84bdfa035001b3c935f3e0dd68adaef
NNA Design <nabilahatiqah12@gmail.com>
2025-10-15 01:39:43 +0800

    Update n98hm1a2n.js

--- cuba1/n98hm1a2n.js
+++ cuba1/n98hm1a2n.js
@@ -31,17 +31,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Slider Gambar (Warna Pilihan)
 const images = ["CodeA.png", "CodeB.png", "CodeC.png", "CodeD.png"];
+const labels = ["Code A", "Code B", "Code C", "Code D"];
 let currentIndex = 0;
 
 function showSlide(index) {
   const slider = document.getElementById("slider-image");
-  if (!slider) return; // elak error jika elemen tak jumpa
+  const label = document.getElementById("slider-label");
+  if (!slider || !label) return;
 
   if (index < 0) currentIndex = images.length - 1;
   else if (index >= images.length) currentIndex = 0;
   else currentIndex = index;
 
   slider.src = images[currentIndex];
+  label.innerText = labels[currentIndex]
 }
 
 function prevSlide() {
@@ -52,6 +55,10 @@ function nextSlide() {
   showSlide(currentIndex + 1);
 }
 
+// Papar slide pertama bila halaman load
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentIndex);
+});
 
 
 // Menu & Section
