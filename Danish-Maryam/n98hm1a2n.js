f5ed548b69bc2c8a3f6fd530e3bbd6722c3aebbf
NNA Design <nabilahatiqah12@gmail.com>
2025-12-10 22:08:22 +0800

    Update n98hm1a2n.js

--- Danish-Maryam/n98hm1a2n.js
+++ Danish-Maryam/n98hm1a2n.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.272071521"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -134,7 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSGWnEmq1UTxj-A_wIvMF4VGaguRrWpS0UOFiUbE3gMgirFKHDqe5XKaFc7ZJ_mrE7WGD7BxpNuyzOK/pub?gid=1468147932&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
