67270238dc678bb81503aa8f17e4a60a696b5395
NNA Design <nabilahatiqah12@gmail.com>
2026-01-04 01:35:41 +0800

    Update n98hm1a2n.js

--- Ridwah-Farhan/n98hm1a2n.js
+++ Ridwah-Farhan/n98hm1a2n.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.388702486"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -134,7 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRUoby7i8iOUvZgi1g4mQHiETBkKHskUNGiJedJovomsNuCeyQWmF82uLZhr3FStPV2FMOhShYmaLJO/pub?gid=1601803613&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
