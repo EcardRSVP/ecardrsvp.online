cea212f22c203cd58dea801696d7aaea909fdaff
NNA Design <nabilahatiqah12@gmail.com>
2026-01-18 18:32:45 +0800

    Update form input name and fetch URL

--- Aine-Zul/n98hm1a2n.js
+++ Aine-Zul/n98hm1a2n.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.1190866702"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -134,7 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vT0jl7Ca_BdqKcQnCi4t0bjCg4xKqTR8UhbCsHfWQZBWJwbb_OU5sZB34OevEoqTi7OpJn5suDTA8nD/pub?gid=535331058&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
