65887832414e5c420706b008a2e306b4eeb36759
NNA Design <nabilahatiqah12@gmail.com>
2026-02-26 01:55:31 +0800

    Update n98hm1a2n.js

--- Izzulfadli-Batrisyia/n98hm1a2n.js
+++ Izzulfadli-Batrisyia/n98hm1a2n.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.56280390"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -134,7 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSbRdyvTYdiRzsd_C7HIaWSsVE8b_fQZkvRtDrKHfJXgim03vMbKrSpomUIvnJNYhBPEGVzVY9eZcD2/pub?gid=138836904&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
