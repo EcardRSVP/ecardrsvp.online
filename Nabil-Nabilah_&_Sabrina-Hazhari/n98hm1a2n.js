eea6e841b4fd74f756f822dc1a09c826be8040ee
NNA Design <nabilahatiqah12@gmail.com>
2026-04-03 21:21:38 +0800

    Update n98hm1a2n.js

--- Nabil-Nabilah_&_Sabrina-Hazhari/n98hm1a2n.js
+++ Nabil-Nabilah_&_Sabrina-Hazhari/n98hm1a2n.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
+      const kehadiran = document.querySelector('input[name="entry.403713139"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -134,7 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vR6jF1W1_HGuLm1-wWw2kdFHZzn1B04JaYhRhD-9MA1htNbZ_Opo44WQrO-Olxl4RKUk3nfwZO_q9il/pub?gid=204198869&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
