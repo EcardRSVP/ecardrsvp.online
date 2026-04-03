bb142a1c39329459d5c749ff84ea5d8c66ea0a75
NNA Design <nabilahatiqah12@gmail.com>
2026-04-03 08:25:25 +0800

    Add files via upload

--- Syamir-Anish/n98hm1a2n.js
+++ Syamir-Anish/n98hm1a2n.js
@@ -54,8 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
  // 📨 Validasi sebelum submit
   if (form) {
     form.addEventListener("submit", function (e) {
-      const kehadiran = document.querySelector('input[name="entry.735779210"]:checked');
-
+      const kehadiran = document.querySelector('input[name="entry.727555102"]:checked');
 
       if (!nama.value.trim() || !kehadiran) {
         e.preventDefault();
@@ -135,8 +134,7 @@ function toggleSection(id) {
 
 
 // ✅ Fetch Ucapan (PapaParse)
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vS4Adj5WIL4EtxSDkiyafHWeBXfMZUHvawzu5BTWX24hl6NrZoCQ3bNfTsgkzg_PyapwRinoQUWtDIg/pub?gid=1203541521&single=true&output=csv
-")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRj4vi5sshHiiuRvtBr7CgNRyk8BLqcP2bSKwSTjMl76FCVnwr05Eow0r8K5Cn1J1N1cI-KFVPpQGq4/pub?gid=241095374&single=true&output=csv")
   .then(response => response.text())
   .then(data => {
     const parsed = Papa.parse(data, { header: true });
